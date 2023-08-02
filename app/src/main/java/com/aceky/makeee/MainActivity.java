682fa9731029e2a08c02438c76652bd8f2fc9edf
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-08-02 20:07:33 +0300

    Init project

---
+++ app/src/main/java/com/aceky/makeee/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aceky.makeee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
