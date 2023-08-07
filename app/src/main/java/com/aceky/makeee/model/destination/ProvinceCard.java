c817b8da71513cdbd6171e7abd1ef9577358781a
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-08-08 00:11:19 +0300

    feat : text

---
+++ app/src/main/java/com/aceky/makeee/model/destination/ProvinceCard.java
@@ -0,0 +1,27 @@
+package com.aceky.makeee.model.destination;
+
+public class ProvinceCard {
+    String name ;
+    int imgId ;
+
+    public ProvinceCard(String name, int imgId) {
+        this.name = name;
+        this.imgId = imgId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
