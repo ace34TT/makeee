5dd035e44015b2f38f3072567cbb7eacd6ce7cdc
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-08-08 00:11:19 +0300

    feat : text

---
+++ app/src/main/java/com/aceky/makeee/model/destination/Hotel.java
@@ -0,0 +1,27 @@
+package com.aceky.makeee.model.destination;
+
+public class Hotel {
+    String name ;
+    String province ;
+
+    public Hotel(String name, String province) {
+        this.name = name;
+        this.province = province;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
